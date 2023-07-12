62989170751362a8ed41e0e2df5058adf9492e9b
leonardofranca <leonardo5155@gmail.com>
2023-07-12 16:48:34 -0300

    Project created

---
+++ src/main/java/com/listadejogos/ListaDeJogosApplication.java
@@ -0,0 +1,13 @@
+package com.listadejogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeJogosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListaDeJogosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/listadejogos/ListaDeJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listadejogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeJogosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
